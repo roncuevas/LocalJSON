be62e1047f50733ef81b8ccc3afb70492230bfe5
Aarón Martínez Cuevas <github@roncuevas.com>
2026-02-11 00:58:47 -0600

    refactor(services): move LocalJSON to Services and conform to protocol

--- Sources/LocalJSON/Services/LocalJSON.swift
+++ Sources/LocalJSON/Services/LocalJSON.swift
@@ -1,18 +1,20 @@
 import Foundation
 
-public final class LocalJSON {
-    public static func getJSON(from file: String) -> Data {
+public final class LocalJSON: LocalJSONProtocol {
+    public init() {}
+
+    public func getJSON(from file: String) throws -> Data {
         let fileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent(file)
-        return try! Data(contentsOf: fileURL)
+        return try Data(contentsOf: fileURL)
     }
 
-    public static func getJSON<T: Decodable>(from file: String, as type: T.Type) throws -> T {
+    public func getJSON<T: Decodable>(from file: String, as type: T.Type) throws -> T {
         let fileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent(file)
         let data = try Data(contentsOf: fileURL)
         return try JSONDecoder().decode(T.self, from: data)
     }
 
-    public static func writeJSON<T: Encodable>(data: T, to path: String) throws {
+    public func writeJSON<T: Encodable>(data: T, to path: String) throws {
         let encoder = JSONEncoder()
         encoder.outputFormatting = .prettyPrinted
         let jsonData = try encoder.encode(data)
